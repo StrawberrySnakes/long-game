61e1bf75b70b4b19d8d1c0441a20016d02226298
Dessa Shapiro <110559444+StrawberrySnakes@users.noreply.github.com>
2024-11-07 09:18:04 -0500

    Finished code

--- LongGame/LongGame/Program.cs
+++ LongGame/LongGame/Program.cs
@@ -5,13 +5,21 @@ namespace LongGame
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        public static void play()
         {
             int score = 0;
 
-            Console.WriteLine("What is your name traveler?");
+            Console.WriteLine("\nWhat is your name traveler?");
             String name = Console.ReadLine();
             Console.WriteLine("Well hello there " + name);
+            if (File.Exists(name + ".txt"))
+            {
+                string oldScore = File.ReadAllText(name + ".txt");
+                int oldScoreNum = int.Parse(oldScore);
+                score = oldScoreNum;
+                Console.WriteLine("You have played before and your old score is: " + score);
+            }
 
             Console.WriteLine("Lets see how many key presses you can make");
 
@@ -19,19 +27,37 @@ namespace LongGame
             {
                 score++;
                 Console.WriteLine("\bYour current score is: " + score);
-
             }
+
             string scoreString = score.ToString();
             /*File.Create("/" + name + ".txt");*/
-            File.WriteAllText(name+".txt", scoreString);  // Create a file and write the content of writeText to it
+            File.WriteAllText(name + ".txt", scoreString);
 
-            string readText = File.ReadAllText("filename.txt");  // Read the contents of the file
-            Console.WriteLine(readText);
+            string readText = File.ReadAllText(name + ".txt");
+            Console.WriteLine("Your final saved score is:" + readText);
+            
+        }
 
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nPress p if you want to play again and q if you want to quit");
+                String playOrQuit = Console.ReadLine();
 
+                if (playOrQuit == "p")
+                {
+                    play();
+                }
+                else
+                {
+                    Console.WriteLine("Thanks for playing");
+                    break;
+                }
+            }
 
-
-
+            
+            
         }
 
         }
