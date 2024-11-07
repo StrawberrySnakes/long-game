ef41106f7541536045aadd224362d443ab7d12c4
Dessa Shapiro <110559444+StrawberrySnakes@users.noreply.github.com>
2024-11-07 08:45:54 -0500

    Added code for keypresses

---
+++ LongGame/LongGame/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace LongGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+
+            Console.WriteLine("What is your name traveler?");
+            String name = Console.ReadLine();
+            Console.WriteLine("Well hello there " + name);
+
+            Console.WriteLine("Lets see how many key presses you can make");
+
+            while (Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+                score++;
+                Console.WriteLine("\bYour current score is: " + score);
+
+            }
+            string scoreString = score.ToString();
+
+            File.WriteAllText(name+".txt", scoreString);
+            
+
+
+
+                
+            }
+
+        }
+    }
+
